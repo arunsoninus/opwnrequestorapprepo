ac7f76502a54535f2ec0df219f8c34e313120d8c
arunsoni Soni <arunsoni@nus.edu.sg>
2026-03-07 11:43:20 +0000

    Changes copied for running in CF

--- opwrequest/webapp/utils/services.js
+++ opwrequest/webapp/utils/services.js
@@ -1,68 +1,86 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device",
-	"./configuration", "./dataformatter"
-], function (JSONModel, Device, Config, Formatter) {
+	"./configuration", "./dataformatter","./headerHelper"
+], function (JSONModel, Device, Config, Formatter,HeaderHelper) {
 	"use strict";
 
 	return {
 
-		fetchLoggedUserToken: function (sThis, callBackFx) {
-			var that = this;
-			var userModel = new JSONModel();
-			userModel.loadData("/services/userapi/currentUser", null, false);
-			sap.ui.getCore().setModel(userModel, "userapi");
-			userModel.dataLoaded().then(function () {
-				var sUserName = sap.ui.getCore().getModel("userapi").getData().name;
-				// sUserName = '<username for debug>';
-				sThis.AppModel.setProperty("/loggedInUserId", sUserName);
-				that._getUserDetails(sThis, sUserName, callBackFx);
-			}.bind(sThis));
-		},
-		_getUserDetails: function (sThis, sUserName, callBackFx) {
-			try {
-				var oHeaders = {
-					"Content-Type": "application/json"
-				};
-				var Payload = {
-					"userName": sUserName
-				};
+		getUserInfoDetails: function (component, callBackFx) {
+			var UtilitySrvModel = component.getComponentModel("UtilitySrvModel");
+			let sUrl = Config.dbOperations.userDetails;
+			var oHeaders = HeaderHelper._headerToken() || null;
 
-				var authModel = new JSONModel();
-				authModel.loadData(Config.dbOperations.authorizeTokenNew, JSON.stringify(Payload), null, "POST", null, null, oHeaders);
-				authModel.attachRequestCompleted(function (oResponse) {
-					var tokenDetails = oResponse.getSource().getData();
-					var userDetails = this.getUserInfoDetails(tokenDetails.token);
-					Object.assign(userDetails, tokenDetails);
-					callBackFx(userDetails);
-				}, this);
-			} catch (e) {
-				sap.m.MessageToast.show("Service temporarily down for maintenance. Excuse us for the inconvenience.");
-			}
+			this._readDataUsingOdataModel(
+				sUrl,
+				UtilitySrvModel,
+				component,
+				[],
+				function (response) {
+					callBackFx(response)
+				}.bind(this),
+				oHeaders,
+				{}
+			);
 		},
-		getLoggedInUserToken: function (sUserName) {
-			var oHeaders = {
-				"Content-Type": "application/json"
-			};
-			var Payload = {
-				"userName": sUserName
-			};
-			var authModel = new JSONModel();
-			authModel.loadData(Config.dbOperations.authorizeTokenNew, JSON.stringify(Payload), null, "POST", null, null, oHeaders);
-			authModel.attachRequestCompleted(function (oResponse) {
-				callBackFx(oResponse.getSource().getData().token);
-			}, this);
 
-			// var authModel = new JSONModel();
-			// authModel.loadData(Config.dbOperations.authorizeToken + sUserName, null, false, "GET", false, false);
-			// return authModel.getData();
-		},
-		getUserInfoDetails: function (userToken) {
-			var userInfoModel = new JSONModel();
-			var oHeaders = Formatter._amendHeaderToken(null, userToken);
-			userInfoModel.loadData(Config.dbOperations.userDetails, null, false, "GET", false, false, oHeaders);
-			return userInfoModel.getData();
-		},
+		// fetchLoggedUserToken: function (sThis, callBackFx) {
+		// 	var that = this;
+		// 	var userModel = new JSONModel();
+		// 	userModel.loadData("/services/userapi/currentUser", null, false);
+		// 	sap.ui.getCore().setModel(userModel, "userapi");
+		// 	userModel.dataLoaded().then(function () {
+		// 		var sUserName = sap.ui.getCore().getModel("userapi").getData().name;
+		// 		// sUserName = '<username for debug>';
+		// 		sThis.AppModel.setProperty("/loggedInUserId", sUserName);
+		// 		that._getUserDetails(sThis, sUserName, callBackFx);
+		// 	}.bind(sThis));
+		// },
+		// _getUserDetails: function (sThis, sUserName, callBackFx) {
+		// 	try {
+		// 		var oHeaders = {
+		// 			"Content-Type": "application/json"
+		// 		};
+		// 		var Payload = {
+		// 			"userName": sUserName
+		// 		};
+
+		// 		var authModel = new JSONModel();
+		// 		authModel.loadData(Config.dbOperations.authorizeTokenNew, JSON.stringify(Payload), null, "POST", null, null, oHeaders);
+		// 		authModel.attachRequestCompleted(function (oResponse) {
+		// 			var tokenDetails = oResponse.getSource().getData();
+		// 			var userDetails = this.getUserInfoDetails(tokenDetails.token);
+		// 			Object.assign(userDetails, tokenDetails);
+		// 			callBackFx(userDetails);
+		// 		}, this);
+		// 	} catch (e) {
+		// 		sap.m.MessageToast.show("Service temporarily down for maintenance. Excuse us for the inconvenience.");
+		// 	}
+		// },
+		// getLoggedInUserToken: function (sUserName) {
+		// 	var oHeaders = {
+		// 		"Content-Type": "application/json"
+		// 	};
+		// 	var Payload = {
+		// 		"userName": sUserName
+		// 	};
+		// 	var authModel = new JSONModel();
+		// 	authModel.loadData(Config.dbOperations.authorizeTokenNew, JSON.stringify(Payload), null, "POST", null, null, oHeaders);
+		// 	authModel.attachRequestCompleted(function (oResponse) {
+		// 		callBackFx(oResponse.getSource().getData().token);
+		// 	}, this);
+
+		// 	// var authModel = new JSONModel();
+		// 	// authModel.loadData(Config.dbOperations.authorizeToken + sUserName, null, false, "GET", false, false);
+		// 	// return authModel.getData();
+		// },
+		// getUserInfoDetails: function (userToken) {
+		// 	var userInfoModel = new JSONModel();
+		// 	var oHeaders = Formatter._amendHeaderToken(null, userToken);
+		// 	userInfoModel.loadData(Config.dbOperations.userDetails, null, false, "GET", false, false, oHeaders);
+		// 	return userInfoModel.getData();
+		// },
 		fetchLoggeInUserImage: function (sThis, callBackFx) {
 			var oPhotoModel = new JSONModel();
 			var sUrl = Config.dbOperations.fetchPhotoUser;
@@ -101,6 +119,50 @@ sap.ui.define([
 					"data:image/png;base64," + oResponse.getSource().getData().d.results[0].photo : null);
 			}.bind(sThis));
 		},
+		getAuditLogData: function (component,selectedReq, callBackFx) {
+			var UtilitySrvModel = component.getComponentModel("UtilitySrvModel");
+			// var staffId = component.AppModel.getProperty("/loggedInUserInfo/userName");
+			var sUrl = Config.dbOperations.auditLogApi;
+
+			var oHeaders = HeaderHelper._headerToken();
+
+			let oParameter = {
+				referenceId: selectedReq.REQUEST_ID,
+				processCode : selectedReq.PROCESS_CODE
+			};
+			this._readDataUsingOdataModel(
+				sUrl,
+				UtilitySrvModel,
+				component,
+				[],
+				function (response) {
+					callBackFx(response.getAuditLogData);
+				}.bind(component),
+				oHeaders,
+				oParameter
+			);
+		},
+		requestUnlock: function (component, selectedReq,callBackFx) {
+			// var staffId = component.AppModel.getProperty("/loggedInUserInfo/userName");
+			var sUrl = Config.dbOperations.lockRelease;
+			var UtilitySrvModel = component.getComponentModel("UtilitySrvModel");
+			var oHeaders = HeaderHelper._headerToken();
+
+			let oParameter = {
+				draftId: selectedReq.REQ_UNIQUE_ID
+			};
+			this._readDataUsingOdataModel(
+				sUrl,
+				UtilitySrvModel,
+				component,
+				[],
+				function (response) {
+					callBackFx(response.releaseLockedRequests);
+				}.bind(component),
+				oHeaders,
+				oParameter
+			);
+		},
 
 		fetchFilterData: function (sThis, oPayload, callBackFx) {
 			var sUrl = Config.dbOperations.fetchFilterLookup;
@@ -121,29 +183,74 @@ sap.ui.define([
 				}
 			}.bind(sThis));
 		},
-		fetchUserImageAsync: function (sThis, staffId) {
-			var oPhotoModel = new JSONModel();
-			var sUrl = Config.dbOperations.fetchPhotoUser;
-			sUrl = sUrl + "?userId=" + staffId;
-			var token = sThis.AppModel.getProperty("/token");
-			var oHeaders = {
-				"Accept": "application/json",
-				"Authorization": "Bearer" + " " + token
+		// fetchUserImageAsync: function (sThis, staffId) {
+		// 	var oPhotoModel = new JSONModel();
+		// 	var sUrl = Config.dbOperations.fetchPhotoUser;
+		// 	sUrl = sUrl + "?userId=" + staffId;
+		// 	var token = sThis.AppModel.getProperty("/token");
+		// 	var oHeaders = {
+		// 		"Accept": "application/json",
+		// 		"Authorization": "Bearer" + " " + token
+		// 	};
+		// 	oPhotoModel.loadData(sUrl, null, false, "GET", null, null, oHeaders);
+		// 	return oPhotoModel.getData().d.results;
+		// },
+		fetchUserPhoto: function (component, callBackFx) {
+			var sUrl = Config.dbOperations.photoApi;
+			var staffId = component.OverviewDashboardModel.getProperty("/staffInfo/STAFF_ID");
+			component.OverviewDashboardModel.setProperty("/loggedInUserStfNumber", staffId);
+
+			var UtilitySrvModel = component.getComponentModel("UtilitySrvModel");
+			var oHeaders = HeaderHelper._headerToken();
+			let oParameter = {
+				userId: staffId
 			};
-			oPhotoModel.loadData(sUrl, null, false, "GET", null, null, oHeaders);
-			return oPhotoModel.getData().d.results;
+
+			this._readDataUsingOdataModel(
+				sUrl,
+				UtilitySrvModel,
+				component,
+				[],
+				function (response) {
+					callBackFx(response?.results[0] || {});
+				}.bind(component),
+				oHeaders,
+				oParameter
+			);
+
 		},
-		fetchTaskProcessDetails: function (sThis, objData, callBackFx) {
-			var oTaskProcessModel = new JSONModel();
+		// fetchTaskProcessDetails: function (sThis, objData, callBackFx) {
+		// 	var oTaskProcessModel = new JSONModel();
+		// 	var sUrl = Config.dbOperations.taskProcessHistoryNew;
+		// 	sUrl = sUrl + objData.REQ_UNIQUE_ID + "&processCode=" + objData.PROCESS_CODE + "&requestId=" + objData.REQUEST_ID;
+		// 	var token = sThis.AppModel.getProperty("/token");
+		// 	var oHeaders = {
+		// 		"Accept": "application/json",
+		// 		"Authorization": "Bearer" + " " + token
+		// 	};
+		// 	oTaskProcessModel.loadData(sUrl, null, false, "GET", null, null, oHeaders);
+		// 	callBackFx(oTaskProcessModel.getData());
+		// },
+		fetchTaskProcessDetails: async function (component, selectedReq, callBackFx) {
+			var UtilitySrvModel = component.getComponentModel("UtilitySrvModel");
 			var sUrl = Config.dbOperations.taskProcessHistoryNew;
-			sUrl = sUrl + objData.REQ_UNIQUE_ID + "&processCode=" + objData.PROCESS_CODE + "&requestId=" + objData.REQUEST_ID;
-			var token = sThis.AppModel.getProperty("/token");
-			var oHeaders = {
-				"Accept": "application/json",
-				"Authorization": "Bearer" + " " + token
+			var oHeaders = HeaderHelper._headerToken();
+			let oParameter = {
+				draftId: selectedReq.REQ_UNIQUE_ID,
+				requestId: selectedReq.REQUEST_ID,
+				processCode: selectedReq.PROCESS_CODE
 			};
-			oTaskProcessModel.loadData(sUrl, null, false, "GET", null, null, oHeaders);
-			callBackFx(oTaskProcessModel.getData());
+			await this._readDataUsingOdataModel(
+				sUrl,
+				UtilitySrvModel,
+				component,
+				[],
+				function (response) {
+					callBackFx(response.getProcessTrackerForNewNChangeRequests);
+				}.bind(component),
+				oHeaders,
+				oParameter
+			);
 		},
 
 		persistCwsRequest: function (component, oHeaders, oPayload, callBackFx) {
@@ -170,7 +277,7 @@ sap.ui.define([
 						callBackFx(oData);
 					}
 				}.bind(component),
-				error: function (oError) {}
+				error: function (oError) { }
 			});
 		},
 		getRequestViewCount: function (serviceUrl, oDataModel, component, aFilter, callBackFx) {
@@ -182,7 +289,7 @@ sap.ui.define([
 
 					}
 				}.bind(component),
-				error: function (oError) {}
+				error: function (oError) { }
 			});
 		},
 		getStatusConfig: function (serviceUrl, oDataModel, component, callBackFx) {
@@ -192,7 +299,7 @@ sap.ui.define([
 						return callBackFx(oData);
 					}
 				}.bind(component),
-				error: function (oError) {}
+				error: function (oError) { }
 			});
 		},
 		_readDataUsingOdataModel: function (serviceUrl, oDataModel, component, aFilter, callBackFx) {
@@ -254,7 +361,7 @@ sap.ui.define([
 			cwSrvModel.update("/AttachmentsDatas('" + attachmentId + "')", oData, {
 				method: "PATCH",
 				headers: oHeaders,
-				success: function () {},
+				success: function () { },
 				error: function (oError) {
 					sap.m.MessageToast.show("Error occurred while updating the status flag.");
 				}
@@ -270,6 +377,85 @@ sap.ui.define([
 			};
 			oValidateModel.loadData(sUrl, null, false, "GET", null, null, oHeaders);
 			return oValidateModel.getData();
+		},
+		_readDataUsingOdataModel: async function (
+			serviceUrl,
+			oDataModel,
+			component,
+			aFilter,
+			callBackFx,
+			headers,
+			urlParameter = {}
+		) {
+			if (!urlParameter) {
+				urlParameter = {};
+			}
+			if (!headers) {
+				headers = HeaderHelper._headerToken();
+			}
+			var p = new Promise(function (resolve, reject) {
+
+				oDataModel.read(serviceUrl, {
+					headers: headers,
+					urlParameters: urlParameter,
+					filters: aFilter,
+					success: function (oData) {
+						resolve(oData);
+					}.bind(component),
+					error: function (oError) {
+						reject(oError);
+					}
+				});
+			});
+			if (typeof callBackFx === "function") {
+				p.then(callBackFx).catch(callBackFx);
+			}
+			return p;
+		},
+		_createDataUsingOdataModelWithRespObject: function (
+			serviceUrl,
+			oDataModel,
+			component,
+			callBackFx,
+			headers,
+			oPayload,
+			bFramePayload = true
+		) {
+			let framePayload = {
+				"data": oPayload
+			}
+			var p = new Promise(function (resolve, reject) {
+				oDataModel.create(serviceUrl, framePayload, {
+					headers: headers,
+					success: function (oData, oResponse) {
+						// Create a complete response object with data, headers, and status
+						var completeResponse = {
+							data: oData,
+							response: oResponse,
+							headers: oResponse ? oResponse.headers : {},
+							status: oResponse ? oResponse.statusCode : 200,
+							success: true
+						};
+						resolve(completeResponse);
+					}.bind(component),
+					error: function (oError) {
+						// Create a complete error response object
+						var errorResponse = {
+							data: null,
+							response: oError,
+							headers: oError ? oError.headers : {},
+							status: oError ? oError.statusCode : 500,
+							success: false,
+							error: oError
+						};
+						reject(errorResponse);
+					}
+				});
+			});
+			if (typeof callBackFx === "function") {
+				p.then(callBackFx).catch(callBackFx);
+			}
+			return p;
 		}
 
 	};
